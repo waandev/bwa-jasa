2c54431b9a611fe9c9d308e1459685699d7e88c4
Muhammad Aswan <muhaswan190897@gmail.com>
2022-07-02 21:10:20 +0800

    add foreign key

--- database/migrations/2022_06_25_083940_create_thumbnail_service_table.php
+++ database/migrations/2022_06_25_083940_create_thumbnail_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdvantageThumbnailServiceTable extends Migration
+class CreateThumbnailServiceTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateAdvantageThumbnailServiceTable extends Migration
      */
     public function up()
     {
-        Schema::create('advantage_thumbnail_service', function (Blueprint $table) {
+        Schema::create('thumbnail_service', function (Blueprint $table) {
             $table->id();
-            $table->integer('service_id')->nullable();
+            $table->foreignId('service_id')->nullable()->index('fk_thumbnail_service_to_service');
             $table->longText('thumbnail');
             $table->softDeletes();
             $table->timestamps();
@@ -29,6 +29,6 @@ class CreateAdvantageThumbnailServiceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('advantage_thumbnail_service');
+        Schema::dropIfExists('thumbnail_service');
     }
 }
