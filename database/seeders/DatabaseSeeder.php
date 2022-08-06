0ed04f4b905682ff36a4e9d3b85c691b71b3dd4d
Muhammad Aswan <muhaswan190897@gmail.com>
2022-08-06 13:07:43 +0800

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $this->call([
+            UsersTableSeeder::class,
+            DetailUserTableSeeder::class,
+            OrderStatusTableSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'              => 'Jhon Doe',
+                'email'             => 'jhon@mail.com',
+                'password'          => Hash::make('Admin@12345'),
+                'remember_token'    => NULL,
+                'created_at'        => date('Y-m-d h:i:s'),
+                'updated_at'        => date('Y-m-d h:i:s'),
+            ],
+            [
+                'name'              => 'Jane Doe',
+                'email'             => 'jane@mail.com',
+                'password'          => Hash::make('Admin@12345'),
+                'remember_token'    => NULL,
+                'created_at'        => date('Y-m-d h:i:s'),
+                'updated_at'        => date('Y-m-d h:i:s'),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
